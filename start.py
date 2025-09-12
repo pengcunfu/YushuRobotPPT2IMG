2d24bcd05e03dadcc810e106591f4147e0faba99
pengcunfu <3173484026@qq.com>
2025-09-12 18:06:46 +0800

    update version2

--- start.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from api import app
-
-os.makedirs("uploads", exist_ok=True)
-os.makedirs("results", exist_ok=True)
-
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=8020)
